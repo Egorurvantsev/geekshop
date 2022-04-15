f9b5908c8456285ed7d68419538f21f516122fad
Egor <egor.urvancev123@gmail.com>
2022-04-15 19:29:26 +0500

    created superuser

--- authnapp/views.py
+++ authnapp/views.py
@@ -12,7 +12,7 @@ def login(request):
     login_form = ShopUserLoginForm(data=request.POST or None)
     next_page = request.GET["next"] if "next" in request.GET.keys() else ""
 
-    if request.method == "POST" and login_form.is_valid():
+    if login_form.is_valid():
         username = request.POST["username"]
         password = request.POST["password"]
 
