3eab6ee4a02e20e5c34088a2c98172da90396c05
mmkndr <egor.urvancev123@gmail.com>
2022-06-07 12:15:06 +0500

    Query renew

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -2,12 +2,9 @@ from basketapp.models import Basket
 
 
 def basket(request):
-    print("context processor basket works")
     basket = []
-
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
-
     return {
         "basket": basket,
     }
